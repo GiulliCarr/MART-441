37a97c0703a5be54cd1bf119c14adf911a1cfe8f
GiulliCarr <123021053+GiulliCarr@users.noreply.github.com>
2025-01-25 12:10:15 -0800

    Add files via upload

---
+++ HW 2/script.js
@@ -0,0 +1,17 @@
+console.log("Websites:");
+console.log("1. https://www.nasa.gov/ - Lowkey mandatory to add since I'm their employee.");
+console.log("2. https://wwwwwwwww.jodi.org/ - I could sit for hours clicking and staring at this.");
+console.log("3. https://youarestandinginanopenfield.com/ - I met these dudes at a market in DTLA; they're dope");
+
+console.log("Favorite Games:");
+console.log("1. Chess - A classic strategy game.");
+console.log("2. The Legend of Zelda: Breath of the Wild - An open-world adventure masterpiece.");
+console.log("3. Settlers of Catan - A fun and strategic board game.");
+
+console.log("Favorite Artists:");
+console.log("1. Keith Haring - Left an everlasting legacy within LGBTQ+ culutre, is a huge inspiration to me asan artist.");
+console.log("2. Mac Miller - An absolute icon; was a pillar of light for many, including me, and still is.");
+console.log("3. Billie Eilish - I genuinely believe her and her brother have paved a new road for music production, and what it means to be uniquely yourself within your work.");
+
+const userAnswer = prompt("What is your favorite song?");
+console.log(`User's fav song is: ${userAnswer}`);
